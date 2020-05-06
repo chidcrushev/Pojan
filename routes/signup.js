40561ed163157c4ac709b76588859a2418875483
Valentine Aduaka <sabival89@gmail.com>
2020-05-05 23:40:04 -0600

    Removed flash declaration in sign up route

--- routes/signup.js
+++ routes/signup.js
@@ -4,7 +4,6 @@ const db        = require('../config/database-config');
 const multer    = require('multer');
 const upload    = multer();
 const helpers   = require('../config/helpers');
-const flash = require('connect-flash');
 
 // Custom MySql Errors
 let sqlErrors = {'1062': {'title': 'Duplicate Entry', 'message': 'Email already exists. Please try again with a different email.'}};
@@ -42,7 +41,6 @@ Router.post('/', upload.none(), (req, res, next) => {
                 res.statusMessage = sqlErrors[error.errno].message;
                 res.status(400).send();
             } else {
-                req.flash('info', 'Flash is back!')
                 res.status(200).json({message: 'Your account has been successfully created'});
             }
         });
